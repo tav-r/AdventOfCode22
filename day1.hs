d6de2f4c1f12f74a7ac7fc1cb851210ee16b5214
David Herrmann <david.herrmann@protonmail.com>
2022-12-18 12:27:04 +0100

    add day1 in haskell

--- day1.hs
+++ day1.hs
@@ -1,11 +1,11 @@
 import Data.List
 
 update :: [Int] -> String -> [Int]
-update [] _ = []
+update [] s = [read s :: Int]
 update xs [] = 0 : xs
 update (x : xs) s = (x + read s :: Int) : xs
 
 main = do
-  res <- reverse . sort . foldl update [0] . lines <$> getContents
+  res <- reverse . sort . foldl update [] . lines <$> getContents
   print $ head res
   print $ sum $ take 3 res
