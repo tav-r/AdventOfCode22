8ab0d604ec94b68e8a9fee8bb4c8479316010060
David Herrmann <david.herrmann@protonmail.com>
2022-12-24 00:50:27 +0100

    day3 in haskell

--- day3.hs
+++ day3.hs
@@ -6,8 +6,8 @@ priority c =
     then 1 + ord c - ord 'a'
     else 27 + ord c - ord 'A'
 
-rucksackVal :: [String] -> Char
-rucksackVal ss = head [c | c <- head ss, all (c `elem`) (drop 1 ss)]
+rucksackPrio :: [String] -> Int
+rucksackPrio ss = priority $ head [c | c <- head ss, all (c `elem`) (drop 1 ss)]
 
 partition :: Int -> [a] -> [[a]]
 partition n [] = []
@@ -15,5 +15,5 @@ partition n as = take n as : partition n (drop n as)
 
 main = do
   puzzleInput <- lines <$> getContents
-  print $ sum $ priority . rucksackVal . (\s -> partition (length s `div` 2) s) <$> puzzleInput
-  print $ sum $ priority . rucksackVal <$> partition 3 puzzleInput
+  print $ sum $ rucksackPrio . (\s -> partition (length s `div` 2) s) <$> puzzleInput
+  print $ sum $ rucksackPrio <$> partition 3 puzzleInput
